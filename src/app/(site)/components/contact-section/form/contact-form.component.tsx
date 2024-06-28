6dfd045ea0e1581d9433157e3d7ea8a504491591
jordan <blvkrvbbit@gmail.com>
2024-06-28 13:00:36 -0600

    changed contact form styling

--- src/app/(site)/components/contact-section/form/contact-form.component.tsx
+++ src/app/(site)/components/contact-section/form/contact-form.component.tsx
@@ -43,36 +43,39 @@ const ContactForm = () => {
       onSubmit={form.handleSubmit(onSubmit)}
       className='space-y-4 max-w-md mx-auto'
     >
-      <div className='font-bold text-2xl tracking-wide'>Get in touch</div>
+      <div className='font-bold text-2xl tracking-wide '>
+        <span className='text-gray opacity-[0.15]'>.</span>getInTouch
+        <span className='text-gray opacity-[0.15]'>()</span>
+      </div>
       <div className='space-y-2'>
-        <label className='font-semibold block' htmlFor='name'>
+        <label className='block' htmlFor='name'>
           Name
         </label>
         <input
           placeholder='Enter your name'
           type='text'
-          className='focus:outline-none w-full p-2 rounded border border-pale-silver'
+          className='focus:outline-none w-full p-2 rounded border border-purple bg-smoky-black text-white'
           {...form.register('name')}
         />
       </div>
       <div className='space-y-2'>
-        <label className='font-semibold block ' htmlFor='email'>
+        <label className='block' htmlFor='email'>
           Email
         </label>
         <input
           placeholder='Enter your email'
           type='text'
-          className='focus:outline-none w-full p-2 rounded border border-pale-silver'
+          className='focus:outline-none w-full p-2 rounded border border-purple bg-smoky-black text-white'
           {...form.register('email')}
         />
       </div>
       <div className='space-y-2'>
-        <label className='font-semibold block ' htmlFor='content'>
+        <label className='block ' htmlFor='content'>
           Inquiry
         </label>
         <textarea
           placeholder='Enter details for your inquiry'
-          className='focus:outline-none w-full border border-pale-silver p-2 h-40'
+          className='focus:outline-none w-full border border-purple bg-smoky-black text-white p-2 h-40'
           {...form.register('content')}
         />
         <button className='text-snow w-full rounded border border-purple p-2 font-semibold tracking-widest'>
