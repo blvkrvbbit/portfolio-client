7bcb06ce1361af9b09912e63b2caa6359c4bfc0f
jordan <blvkrvbbit@gmail.com>
2024-06-28 02:07:09 -0600

    feature: added message routing and prisma

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import db from '@/app/db/db';
+export async function GET(req: Request) {
+  try {
+    const messages = await db.message.findMany();
+    return NextResponse.json({ messages: messages }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, content } = await req.json();
+    const message = await db.message.create({
+      data: { name, email, content },
+    });
+
+    return NextResponse.json({ success: 'Message Sent!' }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
